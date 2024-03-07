30edf4b19873083fe5d00905171f21ba8c522916
Eric Scott <scottericr@gmail.com>
2024-03-07 16:46:43 -0700

    add clunky version of filetype arg

--- R/formats.R
+++ R/formats.R
@@ -1,11 +1,24 @@
-format_terra_rast <- function() {
-    write_fun <- function(object, path) {
-        terra::writeRaster(
-            x = object,
-            filename = path,
-            overwrite = TRUE,
-            filetype = "GTiff" #TODO would love to be able to control this with an argument to format_terra_rast()
-        )
+format_terra_rast <- function(filetype = c("GTiff", "netCDF")) {
+    filetype <- match.arg(filetype)
+    #TODO There's got to be a better way to do this by just passing the arg to writeRaster, but I can't figure it out
+    if(filetype == "GTiff") {
+        write_fun <- function(object, path) {
+            terra::writeRaster(
+                x = object,
+                filename = path,
+                overwrite = TRUE,
+                filetype = "GTiff"
+            )
+        }
+    } else if (filetype == "netCDF") {
+        write_fun <- function(object, path) {
+            terra::writeRaster(
+                x = object,
+                filename = path,
+                overwrite = TRUE,
+                filetype = "netCDF"
+            )
+        }
     }
     targets::tar_format(
         read = function(path) terra::rast(path),
--- tests/testthat/test-formats.R
+++ tests/testthat/test-formats.R
@@ -3,3 +3,7 @@ test_that("formats return output of tar_format()", {
     expect_type(format_terra_rast(), "character")
     expect_match(format_terra_rast(), "^format_custom.+")
 })
+
+test_that("changing filetype does something", {
+    expect_false(format_terra_rast(filetype = "GTiff") == format_terra_rast(filetype = "netCDF"))
+})
