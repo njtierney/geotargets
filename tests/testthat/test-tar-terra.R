c4b67b879d995f006410c3971f7a03a5b2586e09
Eric Scott <scottericr@gmail.com>
2024-03-11 19:33:20 -0700

    add test

--- tests/testthat/test-tar-terra.R
+++ tests/testthat/test-tar-terra.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# test_that() #Included to make RStudio recognize this file as a test
+targets::tar_test("tar_terra_rast() works", {
+    targets::tar_script({
+        list(
+           geotargets::tar_terra_rast(
+                "test",
+                system.file("ex/elev.tif", package="terra") |> terra::rast()
+            )
+        )
+    })
+    targets::tar_make()
+    x <- targets::tar_read(test)
+    expect_s4_class(x, "SpatRaster")
 })
