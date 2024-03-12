0eb01c2bc629c0d985920b027c7ce1027fb12e6b
njtierney <nicholas.tierney@gmail.com>
2024-03-12 15:56:48 +1100

    Add tests and snapshots

---
+++ tests/testthat/_snaps/tar-terra.md
@@ -0,0 +1,32 @@
+# tar_terra_rast() works
+
+    Code
+      x
+    Output
+      class       : SpatRaster 
+      dimensions  : 90, 95, 1  (nrow, ncol, nlyr)
+      resolution  : 0.008333333, 0.008333333  (x, y)
+      extent      : 5.741667, 6.533333, 49.44167, 50.19167  (xmin, xmax, ymin, ymax)
+      coord. ref. : lon/lat WGS 84 (EPSG:4326) 
+      source      : test_terra_rast 
+      name        : elevation 
+      min value   :       141 
+      max value   :       547 
+
+# tar_terra_vect() works
+
+    Code
+      x
+    Output
+       class       : SpatVector 
+       geometry    : polygons 
+       dimensions  : 12, 6  (geometries, attributes)
+       extent      : 5.74414, 6.528252, 49.44781, 50.18162  (xmin, xmax, ymin, ymax)
+       source      : test_terra_vect} (test_terra_vect)
+       coord. ref. : lon/lat WGS 84 (EPSG:4326) 
+       names       :  ID_1   NAME_1  ID_2   NAME_2  AREA   POP
+       type        : <num>    <chr> <num>    <chr> <num> <int>
+       values      :     1 Diekirch     1 Clervaux   312 18081
+                         1 Diekirch     2 Diekirch   218 32543
+                         1 Diekirch     3  Redange   259 18664
+
--- tests/testthat/test-tar-terra.R
+++ tests/testthat/test-tar-terra.R
@@ -1,14 +1,42 @@
 # test_that() #Included to make RStudio recognize this file as a test
 targets::tar_test("tar_terra_rast() works", {
-    targets::tar_script({
-        list(
-           geotargets::tar_terra_rast(
-                "test",
-                system.file("ex/elev.tif", package="terra") |> terra::rast()
-            )
-        )
-    })
-    targets::tar_make()
-    x <- targets::tar_read(test)
-    expect_s4_class(x, "SpatRaster")
+  targets::tar_script({
+    list(
+      geotargets::tar_terra_rast(
+        test_terra_rast,
+        system.file("ex/elev.tif", package = "terra") |> terra::rast()
+      )
+    )
+  })
+  targets::tar_make()
+  x <- targets::tar_read(test_terra_rast)
+  expect_s4_class(x, "SpatRaster")
+  expect_snapshot(
+      x
+  )
+})
+
+targets::tar_test("tar_terra_vect() works", {
+  targets::tar_script({
+    lux_area <- function(projection = "EPSG:4326") {
+      terra::project(
+        terra::vect(system.file("ex", "lux.shp",
+          package = "terra"
+        )),
+        projection
+      )
+    }
+    list(
+      geotargets::tar_terra_vect(
+        test_terra_vect,
+        lux_area()
+      )
+    )
+  })
+  targets::tar_make()
+  x <- targets::tar_read(test_terra_vect)
+  expect_s4_class(x, "SpatVector")
+  expect_snapshot(
+      x
+  )
 })
