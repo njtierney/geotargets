ef4adfda96d9b7823282e6f07e51c4ff3ecc9e9a
Eric Scott <scottericr@gmail.com>
2024-03-11 19:29:29 -0700

    remove test

--- tests/testthat/test-formats.R
+++
@@ -1,9 +0,0 @@
-test_that("formats return output of tar_format()", {
-    expect_type(format_terra_rast, "closure")
-    expect_type(format_terra_rast(), "character")
-    expect_match(format_terra_rast(), "^format_custom.+")
-})
-
-test_that("changing filetype does something", {
-    expect_false(format_terra_rast(filetype = "GTiff") == format_terra_rast(filetype = "netCDF"))
-})
