0d72c8aaca5e2b90389403a85d2eaeca894b0d11
njtierney <nicholas.tierney@gmail.com>
2024-03-25 08:49:56 +1000

    add snapshot tests for check_pkg_installed

---
+++ tests/testthat/_snaps/check-pkg-installed.md
@@ -0,0 +1,8 @@
+# check_pkg_installed() works
+
+    Code
+      check_pkg_installed("asdf")
+    Condition
+      Error:
+      ! package asdf is required
+
---
+++ tests/testthat/test-check-pkg-installed.R
@@ -0,0 +1,7 @@
+test_that("check_pkg_installed() works", {
+  expect_snapshot(
+      error = TRUE,
+      x = check_pkg_installed("asdf")
+  )
+  expect_null(check_pkg_installed("targets"))
+})
