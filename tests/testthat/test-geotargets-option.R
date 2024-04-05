2d7c43fdbb0e32189e91a447828d0db46074b562
Eric Scott <scottericr@gmail.com>
2024-04-05 14:50:58 -0700

    add test with crew workers

---
+++ tests/testthat/test-geotargets-option.R
@@ -0,0 +1,22 @@
+# test_that("geotargets_options_get() retrieves options in correct priority", {
+#     withr::with_envvar(
+#
+#     )
+# })
+
+targets::tar_test("options are distributed to workers", {
+    targets::tar_script({
+        options("geotargets.gdal.raster.driver" = "GeoJSON")
+        targets::tar_option_set(
+            controller = crew::crew_controller_local(workers = 2)
+        )
+        list(
+            targets::tar_target(
+                opt,
+                getOption("geotargets.gdal.raster.driver")
+            )
+        )
+    })
+    targets::tar_make()
+    expect_equal(targets::tar_read(opt), "GeoJSON")
+})
