70675f59f8c51b5f4e4dbc5cfaa9a966def031a2
Eric Scott <scottericr@gmail.com>
2024-04-05 14:53:39 -0700

    add env var test

--- tests/testthat/test-geotargets-option.R
+++ tests/testthat/test-geotargets-option.R
@@ -20,3 +20,20 @@ targets::tar_test("options are distributed to workers", {
     targets::tar_make()
     expect_equal(targets::tar_read(opt), "GeoJSON")
 })
+
+targets::tar_test("env vars are distributed to workers", {
+    targets::tar_script({
+        Sys.setenv("GEOTARGETS_GDAL_RASTER_DRIVER" = "GeoJSON")
+        targets::tar_option_set(
+            controller = crew::crew_controller_local(workers = 2)
+        )
+        list(
+            targets::tar_target(
+                opt,
+                Sys.getenv("GEOTARGETS_GDAL_RASTER_DRIVER")
+            )
+        )
+    })
+    targets::tar_make()
+    expect_equal(targets::tar_read(opt), "GeoJSON")
+})
