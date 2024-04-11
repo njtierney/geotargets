848e463bd2c5fd6e90d377662d765eb1353c4686
Eric Scott <scottericr@gmail.com>
2024-04-11 11:15:36 -0700

    remove commented out code

--- R/AAAA.R
+++
@@ -1,9 +0,0 @@
-# geotargets.env <- new.env(parent = emptyenv())
-#
-# geotargets_env <- function() {
-#     geotargets.env
-# }
-
-# .onAttach <- function(lib, pkg) {
-#     geotargets_option_set()
-# }
