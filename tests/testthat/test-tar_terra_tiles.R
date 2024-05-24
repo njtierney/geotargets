0f78b34e244598996ccf04cf33e298d9fecf3ab7
Eric Scott <scottericr@gmail.com>
2024-05-24 15:50:05 -0700

    add test

---
+++ tests/testthat/test-tar_terra_tiles.R
@@ -0,0 +1,27 @@
+# test_that() #Included to make RStudio recognize this file as a test
+targets::tar_test("tar_terra_tiles() works", {
+    targets::tar_script({
+        library(targets)
+        library(geotargets)
+        library(terra)
+        list(
+            tar_target(
+                my_file,
+                system.file("ex/elev.tif", package="terra"),
+                format = "file"
+            ),
+            tar_terra_rast(
+                my_map,
+                terra::rast(my_file)
+            ),
+            tar_terra_tiles(
+                name = rast_split,
+                raster = my_map,
+                template = terra::rast(ncols = 2, nrows = 2),
+                tiles_dir = "tiles_test"
+            )
+        )
+    })
+    targets::tar_make()
+    expect_true(all(is.na(targets::tar_meta()$error)))
+})
