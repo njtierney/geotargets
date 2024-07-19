e550540f0b712f4d3f8e8747f22ed7a769983cad
Eric Scott <scottericr@gmail.com>
2024-07-19 10:53:46 -0700

    update test

--- tests/testthat/test-tar-terra-tiles.R
+++ tests/testthat/test-tar-terra-tiles.R
@@ -17,15 +17,21 @@ targets::tar_test("tar_terra_tiles() works", {
             tar_terra_tiles(
                 name = rast_split,
                 raster = my_map,
-                ncol = 2,
-                nrow = 2
+                tile_fun = tile_blocksize
+            ),
+            tar_terra_tiles(
+                name = rast_split_n,
+                raster = my_map,
+                tile_fun = \(x) tile_n(x, ncol = 2, nrow = 2)
             )
         )
     })
     manifest <- targets::tar_manifest()
     #check that the command is correct
     expect_equal(manifest[manifest$name == "rast_split_exts", ][["command"]],
-                 "create_tile_exts(my_map, ncol = 2, nrow = 2)")
+                 "tile_blocksize(my_map)")
+    # expect_equal(manifest[manifest$name == "rast_split_n_exts", ][["command"]],
+    #              "tile_n(my_map, ncol = 2, nrow = 2)") #TODO: haven't figured out how to make the manifest look nice with anonymous funs yet
     expect_equal(manifest[manifest$name == "rast_split",][["command"]],
                  "set_window(my_map, terra::ext(rast_split_exts))")
     targets::tar_make()
@@ -50,8 +56,7 @@ targets::tar_test("recombined tiles are equal to original", {
             tar_terra_tiles(
                 name = rast_split,
                 raster = my_map,
-                ncol = 2,
-                nrow = 2
+                tile_fun = tile_blocksize
             )
         )
     })
