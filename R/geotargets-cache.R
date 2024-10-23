3590166df62ac5212ef8f4512cba1907bfaa852f
Eric Scott <scottericr@gmail.com>
2024-10-22 18:55:14 -0700

    re-purpose geotargets_destroy_cache()

--- R/geotargets-cache.R
+++ R/geotargets-cache.R
@@ -1,12 +1,10 @@
 #' Manage geotargets Cache Files
 #'
-#' The `geotargets` cache is a folder containing one subfolder for each target created using `tar_terra_rast_wrap()` method. Each subfolder contains one or more files containing data and metadata to support a `terra` `SpatRaster` object.
-#'
-#' The cache directory can be customized using `geotargets_option_set("cache.dir")` or environment variable `GEOTARGETS_CACHE_DIR`.
+#' The `geotargets` cache is a folder created when the `use_cache` option is TRUE (see [geotargets_option_set()]). The cache directory can be customized using `geotargets_option_set(cache_dir = )` or environment variable `GEOTARGETS_CACHE_DIR`.
 #'
 #' Periodically you may want to purge files in the cache using `geotargets_destroy_cache()`.
 #'
-#' @param name character. Target name (default: `NULL` will delete all target cache files)
+#' @param cache_dir character. Path to a directory to use as a cache
 #' @param init logical. Re-create empty cache directory? Default: `FALSE`
 #'
 #' @return integer. 0 for success, 1 for failure, invisibly.
@@ -15,24 +13,16 @@
 #' @examples
 #' \dontrun{
 #'
-#' # delete cache folder for target named "foo"
-#' geotargets_destroy_cache("foo")
-#'
-#' # create empty folder
-#' geotargets_init_cache("foo")
-#'
-#' # delete and recreate folder
-#' geotargets_destroy_cache("foo", init = TRUE)
-#'
 #' # delete all cache files
 #' geotargets_destroy_cache()
 #'
 #' }
-geotargets_destroy_cache <- function(name = NULL, init = FALSE) {
-    cachedir <- geotargets_option_get("cache.dir")
-    target_cache_dir <- file.path(cachedir %||% "geotargets_cache", name %||% "")
-    res <- unlink(target_cache_dir, recursive = TRUE)
-    if (init) geotargets_init_cache(name = name)
+geotargets_destroy_cache <- function(cache_dir = geotargets_option_get("cache.dir"), init = FALSE) {
+    cache_dir <- cache_dir %||% "_geotargets"
+    #invalidate targets that saved files to the cache_dir
+    targets::tar_invalidate(targets::any_of(list.files(cache_dir)))
+    res <- unlink(cache_dir, recursive = TRUE)
+    if (init) geotargets_init_cache(cache_dir = cache_dir)
     invisible(res)
 }
 
