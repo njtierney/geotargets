10a51719eceef433817b3de4a1814e1fb4a60e45
Eric Scott <scottericr@gmail.com>
2024-10-28 16:00:56 -0700

    implement preserve_metadata argument

--- R/tar-terra-rast.R
+++ R/tar-terra-rast.R
@@ -6,6 +6,12 @@
 #'   to [terra::writeRaster()]
 #' @param gdal character. GDAL driver specific datasource creation options
 #'   passed to [terra::writeRaster()]
+#' @param preserve_metadata logical. When `FALSE` (default), any auxiliary files
+#'   that would be written by [terra::writeRaster()] containing raster metadata
+#'   such as units and datetimes are lost (note that this does not include layer
+#'   names set with `names() <-`).  When `TRUE`, these metadata are retained by
+#'   archiving all written files as a zip file upon writing and unzipping them
+#'   upon reading. This adds extra overhead and will slow pipelines.
 #' @param ... Additional arguments not yet used
 #'
 #' @inheritParams targets::tar_target
@@ -38,6 +44,7 @@ tar_terra_rast <- function(name,
                            pattern = NULL,
                            filetype = geotargets_option_get("gdal.raster.driver"),
                            gdal = geotargets_option_get("gdal.raster.creation.options"),
+                           preserve_metadata = FALSE, #TODO: add a geotargets option for this
                            ...,
                            tidy_eval = targets::tar_option_get("tidy_eval"),
                            packages = targets::tar_option_get("packages"),
@@ -90,19 +97,11 @@ tar_terra_rast <- function(name,
         packages = packages,
         library = library,
         format = targets::tar_format(
-            read = function(path) terra::rast(path),
-            write = function(object, path) {
-                terra::writeRaster(
-                    object,
-                    path,
-                    filetype = filetype,
-                    overwrite = TRUE,
-                    gdal = gdal
-                )
-            },
+            read = tar_rast_read(preserve_metadata = preserve_metadata),
+            write = tar_rast_write(filetype = filetype, gdal = gdal, preserve_metadata = preserve_metadata),
             marshal = function(object) terra::wrap(object),
             unmarshal = function(object) terra::unwrap(object),
-            substitute = list(filetype = filetype, gdal = gdal)
+            substitute = list(filetype = filetype, gdal = gdal, preserve_metadata = preserve_metadata)
         ),
         repository = repository,
         iteration = "list", #only "list" works right now
@@ -117,4 +116,54 @@ tar_terra_rast <- function(name,
         cue = cue,
         description = description
     )
+}
+
+tar_rast_read <- function(preserve_metadata) {
+    if (isTRUE(preserve_metadata)) {
+        function(path) {
+            tmp <- withr::local_tempdir()
+            zip::unzip(zipfile = path, exdir = tmp)
+            terra::rast(file.path(tmp, basename(path)))
+        }
+    } else {
+        function(path) terra::rast(path)
+    }
+}
+
+tar_rast_write <- function(filetype, gdal, preserve_metadata) {
+    if (isTRUE(preserve_metadata)) {
+        function(object, path) {
+            #write the raster in a fresh local tempdir() that disappears when function is done
+            tmp <- withr::local_tempdir()
+            dir.create(file.path(tmp, dirname(path)), recursive = TRUE)
+            terra::writeRaster(
+                object,
+                file.path(tmp, path),
+                filetype = filetype,
+                overwrite = TRUE,
+                gdal = gdal
+            )
+            #package files into a zip file using `zip::zip()`
+            raster_files <- list.files(file.path(tmp, dirname(path)), full.names = TRUE)
+            zip::zip(
+                file.path(tmp, basename(path)),
+                files = raster_files,
+                compression_level = 1,
+                mode = "cherry-pick",
+                root = dirname(raster_files)[1]
+            )
+            #move the zip file to the expected place
+            file.rename(file.path(tmp, basename(path)), path)
+        }
+    } else {
+        function(object, path) {
+            terra::writeRaster(
+                object,
+                path,
+                filetype = filetype,
+                overwrite = TRUE,
+                gdal = gdal
+            )
+        }
+    }
 }
